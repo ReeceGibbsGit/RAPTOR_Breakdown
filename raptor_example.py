4fe852b6395efb42ef387aca31db1ce6acef6d2c
ReeceGibbsGit <reecegibbs93@gmail.com>
2024-05-24 14:21:19 +1200

    Fairy example

---
+++ raptor_example.py
@@ -0,0 +1,172 @@
+import os
+os.environ['OPENAI_API_KEY'] = 'sk-proj-fERHt7WuVXnUpTS1upNKT3BlbkFJqHIMrGosXD1Xo1hxNEJR'
+
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from sklearn.mixture import GaussianMixture
+from langchain_core.runnables import RunnablePassthrough
+from typing import Optional
+import numpy as np
+import umap
+import pandas as pd
+
+loader = DirectoryLoader('data', glob="**/*.txt")
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=200,
+    chunk_overlap=20,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+docs = text_splitter.split_documents(docs)
+texts = [doc.page_content for doc in docs]
+
+
+embedding_model = OpenAIEmbeddings()
+model = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+
+global_embeddings = [embedding_model.embed_query(txt) for txt in texts]
+print('first global embeddings')
+
+# here we reduce the size of the clusters by reducing the dimensionality of the embeddings
+# the idea is to reduce the size of the embeddings whilst still retaining the semantic
+# meaning of our data
+def reduce_cluster_embeddings(
+    embeddings: np.ndarray,
+    dim: int,
+    n_neighbors: Optional[int] = None,
+    metric: str = "cosine",
+) -> np.ndarray:
+    if n_neighbors is None:
+        n_neighbors = int((len(embeddings) - 1) ** 0.5)
+    return umap.UMAP(
+        n_neighbors=n_neighbors, n_components=dim, metric=metric
+    ).fit_transform(embeddings)
+
+dim = 2
+global_embeddings_reduced = reduce_cluster_embeddings(global_embeddings, dim)
+print('reduced global embeddings')
+
+# the purpose of the following two functions is to take the reduced embeddings and 
+# cluster them based on their semantic similarity
+# this is done by essentially plotting the meaning of the embeddings on bell curves
+# and getting the optimal number of clusters based on the BIC model that best fits the data
+def get_optimal_clusters(embeddings: np.ndarray, max_clusters: int = 50, random_state: int = 1234):
+    max_clusters = min(max_clusters, len(embeddings))
+    bics = [GaussianMixture(n_components=n, random_state=random_state).fit(embeddings).bic(embeddings)
+            for n in range(1, max_clusters)]
+    return np.argmin(bics) + 1
+
+def gmm_clustering(embeddings: np.ndarray, threshold: float, random_state: int = 0):
+    n_clusters = get_optimal_clusters(embeddings)
+    gm = GaussianMixture(n_components=n_clusters, random_state=random_state).fit(embeddings)
+    probs = gm.predict_proba(embeddings)
+    labels = [np.where(prob > threshold)[0] for prob in probs]
+    return labels, n_clusters
+
+labels, _ = gmm_clustering(global_embeddings_reduced, threshold=0.5)
+
+# the next step here is to distill the meaning of each of our clusters by 
+# getting an LLM to summarise the meaning of each, this will act as our high-level leaf nodes
+simple_labels = [label[0] if len(label) > 0 else -1 for label in labels]
+
+df = pd.DataFrame({
+    'Text': texts,
+    'Embedding': list(global_embeddings_reduced),
+    'Cluster': simple_labels
+})
+
+def format_cluster_texts(df):
+    clustered_texts = {}
+    for cluster in df['Cluster'].unique():
+        cluster_texts = df[df['Cluster'] == cluster]['Text'].tolist()
+        clustered_texts[cluster] = " --- ".join(cluster_texts)
+    return clustered_texts
+
+clustered_texts = format_cluster_texts(df)
+print('clustered low level embeddings')
+
+template = """You are an assistant to create a detailed summary of the text input prodived.
+Text:
+{text}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model | StrOutputParser()
+
+summaries = {}
+for cluster, text in clustered_texts.items():
+    summary = chain.invoke({"text": text})
+    summaries[cluster] = summary
+
+# now, similar to what we did with our raw information, we want to embed these summaries 
+# so that they are indexed along with our more specific information
+embedded_summaries = [embedding_model.embed_query(summary) for summary in summaries.values()]
+
+embedded_summaries_np = np.array(embedded_summaries)
+
+labels, _ = gmm_clustering(embedded_summaries_np, threshold=0.5)
+
+simple_labels = [label[0] if len(label) > 0 else -1 for label in labels]
+
+clustered_summaries = {}
+for i, label in enumerate(simple_labels):
+    if label not in clustered_summaries:
+        clustered_summaries[label] = []
+    clustered_summaries[label].append(list(summaries.values())[i])
+
+print('clustered higher level embeddings')
+
+# once we have embedded our cluster summaries, we then go and distill the meaning of these
+# into an overall description of our information. The highest level of information in our vectorstore
+# we embed that too
+final_summaries = {}
+for cluster, texts in clustered_summaries.items():
+    combined_text = ' '.join(texts)
+    summary = chain.invoke({"text": combined_text})
+    final_summaries[cluster] = summary
+
+print('clustered highest level embeddings')
+
+# now we prep our data for retrieval by collapsing the data into a single list
+# and house it in our vectorstore
+texts_from_df = df['Text'].tolist()
+texts_from_clustered_texts = list(clustered_texts.values())
+texts_from_final_summaries = list(final_summaries.values())
+
+combined_texts = texts_from_df + texts_from_clustered_texts + texts_from_final_summaries
+
+vectorstore = Chroma.from_texts(texts=combined_texts, embedding=embedding_model)
+
+retriever = vectorstore.as_retriever(search_kwargs={"k": 100})
+
+template = """
+Answer the question based only on the following context:
+{context}
+
+Answer the following question:
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+while True:
+    user_input = input("Ask a question (or enter 'x' to quit): ")
+    if user_input == "x":
+        break
+
+    print(rag_chain.invoke(user_input))
